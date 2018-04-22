385c7d09b0fcfcc3bf871329e90234f05c563a60
cdlscdlscdls <r.bucknumber8@gmail.com>
2018-04-23 01:49:56 +0900

    ejs add & server.js modified

--- practice/server.js
+++ practice/server.js
@@ -1,9 +1,20 @@
-var http = require('http');
-var fs = require('fs');
+var http = require('http'),
+  fs = require('fs'),
+  ejs = require('ejs'),
+  qs = require('querystring');
 var settings = require('./settings');
 var server = http.createServer();
-var msg;
+var template = fs.readFileSync(__dirname + '/ejs/index.ejs', 'utf-8');
+var posts = [];
 
+function renderForm(posts, res){
+  var data = ejs.render(template, {
+    posts: posts
+  });
+  res.writeHead(200,{'Content-Type':'text/html'});
+  res.write(data);
+  res.end();
+}
 server.on('request', function(req,res){
   switch(req.url){
     case '/react':
@@ -18,16 +29,37 @@ server.on('request', function(req,res){
         res.end();
       });
       break;
+    case '/ejs':
+      if(req.method === 'POST'){
+        req.data = '';
+        req.on('readable', function(){
+          req.data += req.read();
+        });
+        req.on('end', function(){
+          var query = qs.parse(req.data);
+          posts.push(query.name);
+          renderForm(posts, res);
+        });
+      }else{
+        renderForm(posts, res);
+      }
+      break;
     case '/ruby':
-      msg = 'ruby is comming soon!'
       res.writeHead(200,{'Content-Type':'text/plain'});
-      res.write(msg);
+      res.write('ruby is comming soon');
       res.end();
       break;
     default:
-      res.writeHead(400, {'Content-Type':'text/plain'});
-      res.write('not found');
-      res.end();
+      fs.readFile( __dirname + '/', 'utf-8', function(err,data){
+        if(err){
+          res.writeHead(400, {'Content-Type':'text/plain'});
+          res.write('not found');
+          return res.end();
+        }
+        res.writeHead(400, {'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+      });
       break;
   }  
 });
